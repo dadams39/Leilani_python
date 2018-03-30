df297c189705f64d7a8374d68591b98f6e1d7804
dadams39 <da469@cornell.edu>
2018-03-30 18:25:46 -0400

    Test file updated.

--- test_factorial.py
+++ test_factorial.py
@@ -45,8 +45,41 @@ class Test_Factorial(unittest.TestCase):
             self.assertTrue( temp == 1 )
             self.assertTrue( errors == None )
         except Exception as e:
-            print(e)
-            proc.kill()
-            self.assertTrue(False)
+            msg = "Factorial 0: failed\nYour output: %s" % ( out.decode("utf-8") )
+            self.assertTrue(False, msg)
         print("Test  passed: {}".format(inspect.stack()[0][3]))
 
+    def test_4_fact_2(self):
+        print("Test started: {}".format(inspect.stack()[0][3]))
+        self.assertTrue(self.has_file, 
+                "File: 'fact.py' required")
+        proc = subprocess.Popen(["python3", "fact.py", "0"],\
+            stdout=subprocess.PIPE)
+        try:
+            out, errors = proc.communicate()
+            temp = int(out)
+            self.assertTrue( temp == 2 )
+            self.assertTrue( errors == None )
+        except Exception as e:
+            msg = "Factorial 1: failed\nYour output: %s" % ( out.decode("utf-8") )
+            self.assertTrue(False, msg)
+        print("Test  passed: {}".format(inspect.stack()[0][3]))
+
+    def test_5_fact_6(self):
+        print("Test started: {}".format(inspect.stack()[0][3]))
+        self.assertTrue(self.has_file, 
+                "File: 'fact.py' required")
+        proc = subprocess.Popen(["python3", "fact.py", "0"],\
+            stdout=subprocess.PIPE)
+        try:
+            out, errors = proc.communicate()
+            temp = int(out)
+            self.assertTrue( temp == 720 )
+            self.assertTrue( errors == None )
+        except Exception as e:
+            msg = "Factorial 6: failed\nYour output: %s" % ( out.decode("utf-8") )
+            self.assertTrue(False, msg)
+        print("Test  passed: {}".format(inspect.stack()[0][3]))
+
+if __name__ == '__main__':
+        unittest.main()
